b3cbcd3fd2891ebfa333c2a5bb90b98bc0dd93db
Milon <jan.milon.56@gmail.com>
2023-02-14 13:23:19 +0530

    pagination UI done

--- src/loaders/productsAndCartLoader.js
+++ src/loaders/productsAndCartLoader.js
@@ -4,11 +4,9 @@ export const productsAndCartLoader = async () => {
     // get Products
     const productsData = await fetch('http://localhost:5000/products');
     const {products} = await productsData.json();
-
     // get cart
     const savedCart = getStoredCart();
     const initialCart = [];
-    
     for (const _id in savedCart) {
         const addedProduct = products.find(product => product._id === _id);
         if (addedProduct) { 
@@ -17,6 +15,5 @@ export const productsAndCartLoader = async () => {
             initialCart.push(addedProduct);
         }
     }
-
     return { products: products, initialCart: initialCart };
 }
