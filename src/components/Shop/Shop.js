ed44f622bef4fcf8f991644b63112b95acc83dc7
Milon <jan.milon.56@gmail.com>
2023-03-16 01:55:51 +0530

    paginations works

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -10,11 +10,22 @@ import Product from "../Product/Product";
 import "./Shop.css";
 
 const Shop = () => {
-  const { products, count } = useLoaderData();
+  // const { products, count } = useLoaderData();
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
   const [cart, setCart] = useState([]);
   const [page, setPage] = useState(0);
   const [size, setSize] = useState(10);
   const pages = Math.ceil(count / size);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/products?page=${page}&size=${size}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data.products);
+        setCount(data.count);
+      });
+  }, [page, size]);
 
   const clearCart = () => {
     setCart([]);
@@ -91,6 +102,12 @@ const Shop = () => {
             </button>
           );
         })}
+        <select className="" name="" id="" onChange={event => setSize(event.target.value)}>
+          <option value="5">5</option>
+          <option value="10" selected>10</option>
+          <option value="15">15</option>
+          <option value="20">20</option>
+        </select>
       </div>
     </div>
   );
